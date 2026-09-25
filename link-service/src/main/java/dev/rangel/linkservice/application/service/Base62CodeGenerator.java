e4d4869e992f891605f0c756e895fecc08eff3e3
Rangel <joaquimrangel.dev@gmail.com>
2026-09-25 09:30:33 -0300

    feat(link-service): add base62 code generator

--- link-service/src/main/java/dev/rangel/linkservice/application/service/Base62CodeGenerator.java
+++ link-service/src/main/java/dev/rangel/linkservice/application/service/Base62CodeGenerator.java
@@ -1,4 +1,20 @@
-package dev.rangel.linkservice.domain.service;
+package dev.rangel.linkservice.application.service;
 
+import org.springframework.stereotype.Component;
+import java.security.SecureRandom;
+
+@Component
 public class Base62CodeGenerator {
+
+    private static final String BASE62_ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private final SecureRandom random = new SecureRandom();
+
+    public String generate(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(BASE62_ALPHABET.length());
+            sb.append(BASE62_ALPHABET.charAt(index));
+        }
+        return sb.toString();
+    }
 }
